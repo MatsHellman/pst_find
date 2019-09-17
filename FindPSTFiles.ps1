19f8a075ad8d7f5a367049a1ee2317cd905673e6
Mats Hellman <mahellma@heimdall.local>
2019-09-17 12:46:08 +0300

    debug code removed from findpst.ps1

--- FindPSTFiles.ps1
+++ FindPSTFiles.ps1
@@ -1,13 +1,12 @@
 $Search = $env:USERPROFILE + "/*.pst"
 $OutFile = $env:USERPROFILE + "/FindPST.csv"
 
-if (Get-ChildItem $OutFile){
+if (Get-ChildItem $OutFile -ErrorAction SilentlyContinue){
     Remove-Item $OutFile
 }
 
 
-$Result = Get-ChildItem -Recurse -Path $Search
-Write-Host $Result
+$Result = Get-ChildItem -Recurse -Path $Search -ErrorAction SilentlyContinue
 
 if ( $Result ){
     $Result | ForEach-Object { Add-Content -Path  $OutFile -Value $_ }
